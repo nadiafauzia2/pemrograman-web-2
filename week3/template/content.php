530b98ca2a452794e7920b50a610d85be8a11f5d
nadiafauzia2 <115348655+nadiafauzia2@users.noreply.github.com>
2023-03-10 12:58:50 +0700

    Form Register

---
+++ week3/template/content.php
@@ -0,0 +1,7 @@
+<div id="layoutSidenac_content">
+    <main>
+        <div class="container-fluid px-4px">
+            <h1 class="mt-4">Dashboard</h1>
+            <p>Hello Wordl ini adalah dashboard</p>
+    </div>
+ </main>
