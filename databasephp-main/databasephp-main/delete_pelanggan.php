82916f68f7249b3c11d69d7a76d6bb2301624892
nadiafauzia2 <nadiafauzia02@gmail.com>
2023-03-29 10:20:09 +0700

    upload week 5

---
+++ databasephp-main/databasephp-main/delete_pelanggan.php
@@ -0,0 +1,11 @@
+<?php include "dbkoneksi.php";?>
+ <?php
+
+        $id_edit = $_GET['iddel'];
+        var_dump($id_edit);
+        $sql = "DELETE from pelanggan where id=?";
+	 	$st = $dbh->prepare($sql);
+        $st->execute([$id_edit]);
+	 //echo $_POST['nama'];
+   	header("location: list_pelanggan.php");
+ ?> 
